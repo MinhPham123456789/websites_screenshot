c208ac53bef6d7b70455e64bd07e2b57d6eb3b05
Haha Haha <haha@haha.haha>
2024-10-18 21:12:01 -0500

    Added read URLs in text file, help menu, args, arrange output directories

--- websites_screenshot.py
+++ websites_screenshot.py
@@ -1,18 +1,50 @@
 import asyncio
 from playwright.async_api import async_playwright
+import os
+import argparse
+import sys
 
 """
 Note:
-+ Make the output dir to be the domain name service of the website
++ Make the output dir to be the domain name service of the website (the output_dir param of the process_urls())
 + Make the output dir contains a subdir for images and a HTML report
 + Make the screenshots' name contains the path of the URLs
-+ Make the script taking user input in CLI
-+ Make help menu
++ Random User Agent
 + Generate HTML report
 + Look into window size 
 + Look into screenshot size 
     + Source: https://stackoverflow.com/questions/74559415/how-to-take-a-screenshot-by-passing-coordinates-and-dimensions-in-playwright
 """
+
+def check_and_create_directory(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory '{directory_path}' created.")
+    else:
+        print(f"Directory '{directory_path}' already exists.")
+    image_path = f'{directory_path}/images'
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
+        print(f"Directory '{image_path}' created.")
+    else:
+        print(f"Directory '{image_path}' already exists.")
+    return directory_path, image_path
+
+def read_file_to_array(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            # Strip newline characters from each line
+            lines = [line.strip() for line in lines]
+            return lines
+
+    except FileNotFoundError:
+        print("The file was not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
 async def take_screenshot(url, filename):
     """
     Take a screenshot of a website and save it to a file
@@ -48,9 +80,10 @@ async def process_urls(urls, threads, output_dir):
         threads (int): number of requests simultaneously
     """
     tasks = []
+    base_path, image_path = check_and_create_directory(output_dir)
     for i, url in enumerate(urls):
         filename = f"screenshot_{i + 1}.png" # Create a filename for each screenshot
-        save_screenshot_to = f'{output_dir}/{filename}'
+        save_screenshot_to = f'{image_path}/{filename}'
         tasks.append(take_screenshot(url, save_screenshot_to))
         # If we have 10 tasks, run them concurrently
         if len(tasks) == threads:
@@ -60,13 +93,22 @@ async def process_urls(urls, threads, output_dir):
     if tasks:
         await asyncio.gather(*tasks)
 
-# Example usage
-# Must beginning with http protocol
-urls = [
-    "https://www.google.com",
-    "https://www.youtube.com",
-    "https://www.stackoverflow.com"
-]
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Take URLs\' screenshot with threading.')
+    # Add arguments
+    parser.add_argument('-urls-file', type=str, required=True, help='Path to the file containing URLs')
+    parser.add_argument('-threads', type=int, default=2, help='Number of threads to use (default: 2)')
+    parser.add_argument('-output', type=str, default=".",
+    help='Directory to store output, a new subdirectory will be created to store data (default: .)(your current working directory)')
 
-# Run the async function
-asyncio.run(process_urls(urls, 2, "./sample_output"))
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Read the urls
+    # Must beginning with http protocol
+    urls = read_file_to_array(args.urls_file)
+
+    # Run the async function
+    output_dir = f'{args.output}/sample_output'
+    asyncio.run(process_urls(urls, args.threads, output_dir))
